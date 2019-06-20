4a3d3c4462f02e767912f77395560b85619889df
Chris <christophertberg@gmail.com>
2019-06-19 19:55:08 -0700

    Added fixtures directory for testing picture upload and added tests for updating user fields.

--- tests/user.test.js
+++ tests/user.test.js
@@ -79,7 +79,7 @@ test('Should not login nonexistent user', async () => {
 test('Should get profile for user', async () => {
     await request(app)
         .get('/users/me')
-        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`) // Authenticates user.
         .send()
         .expect(200);
 });
@@ -112,4 +112,41 @@ test('Should not delete account for unauthenticated user', async () => {
         .delete('/users/me')
         .send()
         .expect(401);
+});
+
+// Test that a profile picture is uploaded.
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg') // Provided by supertest allowing us to attach files.
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+    const response = await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Bob'
+        })
+        .expect(200);
+    
+    const user = await User.findById(userOneId);
+    expect(user.name).toEqual('Bob');
+    
+    //expect(response.body.name).not.toBe(userOne.name);
+});
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Boston'
+        })
+        .expect(400);
 });
